09b1758ad95421996861f8ef8b8665fddb02b02f
PhilT <thang.zoramthanga@gmail.com>
2023-09-09 16:27:42 -0400

    Initial Commit

---
+++ Laiholh/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Laiholh
+//
+//  Created by macos on 9/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Laiholh/LaiholhApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaiholhApp.swift
+//  Laiholh
+//
+//  Created by macos on 9/9/23.
+//
+
+import SwiftUI
+
+@main
+struct LaiholhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
